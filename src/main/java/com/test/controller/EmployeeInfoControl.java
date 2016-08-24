356ad3cd4cf75ea68fd64e42238bd4ed1272ad5d
王敏 <wangmin@wangmindeMacBook-Pro.local>
2016-08-24 17:32:37 +0800

    integrated log

--- src/main/java/com/test/controller/EmployeeInfoControl.java
+++ src/main/java/com/test/controller/EmployeeInfoControl.java
@@ -3,11 +3,13 @@ package com.test.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSON;
 import com.test.bean.EmployeeDto;
 import com.test.service.EmployeeService;
 import com.test.utilty.HttpUtils;
@@ -16,16 +18,17 @@ import com.test.utilty.HttpUtils;
 @RequestMapping("/emplayee.do")
 public class EmployeeInfoControl {
 
+	public final Logger logger = LoggerFactory.getLogger(EmployeeInfoControl.class);
+
 	@Autowired
 	EmployeeService employeeService;
 
 	@RequestMapping(params = "method=showEmplayeeInfo")
 	public void showEmplayeeInfo(HttpServletRequest request, HttpServletResponse response, EmployeeDto dto) {
-		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>我进controller了");
+		logger.info("start method : {}", "showEmplayeeInfo");
 		long id = Long.parseLong("3");
 		EmployeeDto employee = employeeService.getEmployeeById(id);
 
-		HttpUtils.writeToPage(response, JSONObject.toJSONString(employee));
-
+		HttpUtils.writeToPage(response, JSON.toJSONString(employee));
 	}
 }
--- src/main/java/com/test/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/test/service/impl/EmployeeServiceImpl.java
@@ -1,5 +1,7 @@
 package com.test.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,12 +11,17 @@ import com.test.service.EmployeeService;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
+	
+	public Logger logger = LoggerFactory.getLogger(EmployeeServiceImpl.class);
 
 	@Autowired
 	EmployeeDao employeeDto;
 
+	@Override
 	public EmployeeDto getEmployeeById(Long id) {
-		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>我进service了");
+		logger.info("start method : {} param id : {}","getEmployeeById",id);
+		int a = 1;
+		int b = a / 0;
 		return employeeDto.selectByPrimaryKey(id);
 	}
 }
